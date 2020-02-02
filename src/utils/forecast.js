fd2ead012b37121c6a3815b7f0e2c93078a00b6d
adam <adamba433@gmail.com>
2020-02-02 17:26:30 +0200

    added moonPhase information to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            let senRise=new Date(body.daily.data[0].sunsetTime)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. moonPhase: '+ body.daily.data[0].moonPhase)
         }
     })
 }
